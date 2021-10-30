fdbc4396313333c319f258ee0c475d364cdbd717
Hoang Yen <yenvhgcs18147@fpt.edu.vn>
2021-10-30 21:26:38 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {DataProvider} from '../store/GlobalState'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DataProvider>
+      <Component {...pageProps} />
+    </DataProvider>
+  )
 }
 
 export default MyApp
